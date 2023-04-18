4d3310351d8d7d8220f4b16752dd64164f7e562b
Kübra Aykan <aykan.kubra99@gmail.com>
2023-04-18 15:34:06 +0300

    added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int k, total = 0, numbers = 0;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Sayı giriniz:");
+        k = inp.nextInt();
+
+        for (int i = 0; i <= k; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                total += i;
+                numbers++;
+
+            }
+        }
+        if (numbers == 0) {
+            System.out.println("Girilen sayı aralığında 3 ve 4'e tam bölünen bir sayı yok.");
+        } else {
+            double average = (double) total / numbers;
+            System.out.println("Ortalama değeri:" + average);
+        }
     }
 }
